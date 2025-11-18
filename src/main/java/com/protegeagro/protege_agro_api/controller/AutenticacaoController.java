7dedad1576f5fbf2a31e19a94d6e801e3b4432a2
Juliana Comparoto ᓚ₍⑅^..^₎♡~ <jcb5@cesar.school>
2025-11-17 21:35:54 -0300

    add url front

--- src/main/java/com/protegeagro/protege_agro_api/controller/AutenticacaoController.java
+++ src/main/java/com/protegeagro/protege_agro_api/controller/AutenticacaoController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "https://protege-agro-front.onrender.com")
 @RestController
 @RequestMapping("/api/auth")
 public class AutenticacaoController {
