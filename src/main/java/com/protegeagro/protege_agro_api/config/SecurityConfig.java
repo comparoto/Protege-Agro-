62d450b048e7658279b79443fd3429344ea3e5a5
Juliana Comparoto ᓚ₍⑅^..^₎♡~ <jcb5@cesar.school>
2025-11-23 21:00:22 -0300

    atualizando ambiente de producao

--- src/main/java/com/protegeagro/protege_agro_api/config/SecurityConfig.java
+++ src/main/java/com/protegeagro/protege_agro_api/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -42,7 +43,7 @@ public class SecurityConfig {
                         .requestMatchers("/h2/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
         return http.build( );
     }
