f1c0bf69d2d80ca31e7adee603049a1e2c819b96
Juliana Comparoto ᓚ₍⑅^..^₎♡~ <jcb5@cesar.school>
2025-11-26 00:20:22 -0300

    Atualizando PostGreSQL

--- src/main/java/com/protegeagro/protege_agro_api/model/Usuario.java
+++ src/main/java/com/protegeagro/protege_agro_api/model/Usuario.java
@@ -2,8 +2,6 @@ package com.protegeagro.protege_agro_api.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -12,15 +10,11 @@ import lombok.Data;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
 
     @Column(nullable = false)
     private String nome;
-
-
-    @Column(nullable = false, unique = true)
-    private String email;
 
     @Column(nullable = false)
     private String senha;
--- src/main/java/com/protegeagro/protege_agro_api/repository/UsuarioRepository.java
+++ src/main/java/com/protegeagro/protege_agro_api/repository/UsuarioRepository.java
@@ -4,7 +4,7 @@ import com.protegeagro.protege_agro_api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
 
     Optional<Usuario> findByEmail(String email);
 }
